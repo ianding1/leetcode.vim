ianding1/leetcode.vim

commit 2e000414e0d81782dd0842e08d320f80ba7af2dc

Add buffer local options ideal for description

autoload/leetcode.vim
@@ -733,6 +733,8 @@ function! leetcode#ResetSolution(with_latest_submission) abort
 
     call append('$', output)
     setlocal nomodifiable
+    setlocal buftype=nofile
+    setlocal nospell
 
     silent! normal! ggdd
     execute 'wincmd j'

commit ad63289e7889924e441f9599fcde64b551a5635a

Fix bugs caused by #47

autoload/leetcode.py
@@ -258,6 +258,7 @@ def get_problem(slug):
     body = {'query': '''query getQuestionDetail($titleSlug : String!) {
   question(titleSlug: $titleSlug) {
     questionId
+    questionFrontendId
     title
     content
     stats
@@ -286,6 +287,7 @@ def get_problem(slug):
     soup = BeautifulSoup(content, features='html.parser')
     problem = {}
     problem['id'] = q['questionId']
+    problem['fid'] = q['questionFrontendId']
     problem['title'] = q['title']
     problem['slug'] = slug
     problem['level'] = q['difficulty']

autoload/leetcode.vim
@@ -720,9 +720,8 @@ function! leetcode#ResetSolution(with_latest_submission) abort
     endfor
     call add(output, s:CommentEnd(filetype))
     call append('$', code)
-    silent! normal! gggqG
 
-    let problem_desc_file_name = printf('[DESCRIPTION] %s.%s', problem['id'], problem_slug)
+    let problem_desc_file_name = printf('[DESCRIPTION] %s.%s', problem['fid'], problem_slug)
     if buflisted(problem_desc_file_name)
         execute bufnr(problem_desc_file_name) . 'buffer'
         return